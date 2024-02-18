fdce2dc931cf3e8ff4969d115269c39442bf80a7
SOXA-A <aleksandre1809@gmail.com>
2024-02-19 00:08:04 +0400

    second commit

---
+++ day 1/home work/lessonme1.py
@@ -0,0 +1,78 @@
+from turtle import *
+
+
+
+
+#we want to paint a house
+
+width(7)
+
+color("purple")
+begin_fill()
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+end_fill()
+#end of square
+#drawing a door
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)      
+right(90)
+forward(120)      
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+penup()
+goto(20, 40)
+pendown()
+color("brown")
+begin_fill()
+left(120)
+forward(30)
+left(90)
+forward(50)
+left(90)
+forward(30)
+left(90)
+forward(50)
+left(90)
+end_fill()
+
+penup()
+goto(150, 40)
+pendown()
+begin_fill()
+forward(30)
+left(90)
+forward(50)
+left(90)
+forward(30)
+left(90)
+forward(50)
+end_fill()
+
+
+exitonclick()
+
