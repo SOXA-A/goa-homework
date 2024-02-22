55569a81da707b25eb9456cd2112593d53d0c99c
SOXA-A <aleksandre1809@gmail.com>
2024-02-23 01:13:43 +0400

    second commit

---
+++ Day1/lessen1.py
@@ -0,0 +1,18 @@
+name = "aleksandre" # ეს არის რ
+surname = 'sokhadze'
+age = 10
+height = 153,5
+knoows_programming = True
+is_ugly = False
+
+#print(name+" " +surname)
+
+#print(name + age)
+
+#print(type(age)) 
+#print(type(name))
+#print(type(height))
+#print(type(is_ugly))
+print(name + "  " + surname + " " + str (age) + " " + str(height)) 
+
+
